aca744cf4b25b82973c8f32b038242e52df28928
aretu-admin <ste.aretu@gmail.com>
2025-10-20 12:40:55 +0200

    included julia models path

--- benchmark_script.m
+++ benchmark_script.m
@@ -6,8 +6,8 @@ benchmark_list={
     "shale-08-10-02","Shale08ss_injection_rate_10_alpha_02.dat";...
     "shale-09-02-02","Shale09ss_injection_rate_02_alpha_02.dat";...
     "shale-09-10-02","Shale09ss_injection_rate_10_alpha_02.dat";...
-    "shale-09-02-02-old","Shale09ss_injection_rate_02_alpha_02.dat";...
-    "shale-09-10-02-old","Shale09ss_injection_rate_10_alpha_02.dat";...
+    % "shale-09-02-02-old","Shale09ss_injection_rate_02_alpha_02.dat";...
+    % "shale-09-10-02-old","Shale09ss_injection_rate_10_alpha_02.dat";...
     "carbonate-08-02-00","Carbonate08ss_injection_rate_02_alpha_00.dat";...
     "carbonate-08-02-03","Carbonate08ss_injection_rate_02_alpha_03.dat";...
     "carbonate-08-02-04","Carbonate08ss_injection_rate_02_alpha_04.dat";...
@@ -29,8 +29,10 @@ for j=1:size(benchmark_list,1)
     name2=char(benchmark_list{j,2});
 
     parameters=get_parameters(name);
-    
+
     shift=100; %before: 100
+
+    disp("Import and plot JR models")
 
     if contains (name2,'arbonate')
         jr=JRcarb2mat(['benchmark\',name2]);
@@ -46,15 +48,46 @@ for j=1:size(benchmark_list,1)
     
     plotter(jr,parameters);
 
+    % load and plot matlab model
+    disp("Import and plot matlab models")
+
     globaloutput=load(['benchmark\',name,'model.mat']);
     
     globaloutput=globaloutput.globaloutput;
 
     plotter(globaloutput,parameters);
     hold on
+    
+    % load and plot julia model if available
+    juliamodel = false;
+    juliamodel2 = false;
+    try
+        disp("Import and plot julia models")
+        juliamodel=importdata(['benchmark\',name,'rt.csv']);
+        
+        juliamodel=juliamodel.data;
+
+        plotter(juliamodel,parameters)
+        hold on
+        
+        juliamodel = true;
+    end
+
+    try
+        disp("Import and plot julia models")
+        juliamodel=importdata(['benchmark\',name,'.csv']);
+        
+        juliamodel=juliamodel.data;
+
+        plotter(juliamodel,parameters)
+        hold on
+        
+        juliamodel2 = true;
+    end
 
     sgtitle(name)
     fig=gcf;
+    alpha=0.75;
 
     for i=1:size(fig.Children,1)
         if fig.Children(i).Type == "axes"
@@ -62,16 +95,63 @@ for j=1:size(benchmark_list,1)
             gca.FontSize=14;
             gca.FontName='Arial';
 
-            gca.Children(2).DisplayName='JR23';
-            gca.Children(2).LineStyle='none';
-            gca.Children(2).Marker='o';
-            gca.Children(2).Color='black';
+            if xor(juliamodel,juliamodel2)
+                gca.Children(3).DisplayName='JR23';
+                gca.Children(3).LineStyle='none';
+                gca.Children(3).Marker='o';
+                gca.Children(3).Color='black';
 
-            gca.Children(1).DisplayName='SA';
-            gca.Children(1).LineStyle='-';
-            gca.Children(1).LineWidth=2;
-            gca.Children(1).Color='red';
-            uistack(gca.Children(1),"top");
+                gca.Children(2).DisplayName='SA - matlab';
+                gca.Children(2).LineStyle='-';
+                gca.Children(2).LineWidth=2;
+                gca.Children(2).Color='red';
+                gca.Children(2).Color=[gca.Children(2).Color,alpha];
+    
+                gca.Children(1).DisplayName='SA - julia';
+                gca.Children(1).LineStyle='-';
+                gca.Children(1).LineWidth=2;
+                gca.Children(1).Color='cyan';
+                gca.Children(1).Color=[gca.Children(1).Color,alpha];
+    
+            
+            elseif (juliamodel==true) && (juliamodel2==true)
+                gca.Children(4).DisplayName='JR23';
+                gca.Children(4).LineStyle='none';
+                gca.Children(4).Marker='o';
+                gca.Children(4).Color='black';
+
+                gca.Children(3).DisplayName='SA - matlab';
+                gca.Children(3).LineStyle='-';
+                gca.Children(3).LineWidth=2;
+                gca.Children(3).Color='red';
+                gca.Children(3).Color=[gca.Children(3).Color,alpha];
+
+                gca.Children(2).DisplayName='SA - julia - rt';
+                gca.Children(2).LineStyle='-';
+                gca.Children(2).LineWidth=2;
+                gca.Children(2).Color='cyan';
+                gca.Children(2).Color=[gca.Children(2).Color,alpha];
+
+                gca.Children(1).DisplayName='SA - julia';
+                gca.Children(1).LineStyle='-';
+                gca.Children(1).LineWidth=2;
+                gca.Children(1).Color='green';
+                gca.Children(1).Color=[gca.Children(1).Color,alpha];
+                
+            elseif (juliamodel == false) && (juliamodel2 == false)
+                gca.Children(2).DisplayName='JR23';
+                gca.Children(2).LineStyle='none';
+                gca.Children(2).Marker='o';
+                gca.Children(2).Color='black';
+    
+                gca.Children(1).DisplayName='SA - matlab';
+                gca.Children(1).LineStyle='-';
+                gca.Children(1).LineWidth=2;
+                gca.Children(1).Color='red';
+                gca.Children(1).Color=[gca.Children(1).Color,alpha];
+            end
+            % uistack(gca.Children(2),"top");
+            % uistack(gca.Children(1),"top");
 
             if or(fig.Children(i).Title.String == "Sliprate",fig.Children(i).Title.String == "Slip")
                 gca.YScale="log";
--- plot_script.m
+++ plot_script.m
@@ -1,13 +1,31 @@
 close all
-name="dummy-s";
+name="mcf-055-01-00-000000-1a";
+% name="mcf-055-01-00-000000-000001-2E-12-2a";
 name=char(name);
 
+% model parameters
+
 parameters=get_parameters(name);
+
+%% matlab
+% load matlab model results
 
 globaloutput=load(['results\',name,'model.mat']);
 
 globaloutput=globaloutput.globaloutput;
 
+% plot matlab model results
 comparison_plotter(globaloutput,parameters);
 
 dimplotter(globaloutput,parameters);
+
+%% julia
+% load julia model result
+
+globaloutput2=importdata(['..\julia-tests\results\',name,'rt.csv']);
+globaloutput2=globaloutput2.data;
+
+% plot julia model results
+comparison_plotter(globaloutput2,parameters);
+
+dimplotter(globaloutput2,parameters);
